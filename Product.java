cb001d319acaff75d9c3936115ca9f362ae372f4
Anmolpreet1311 <85575540+Anmolpreet1311@users.noreply.github.com>
2022-10-05 21:58:56 -0400

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,50 @@
+/**
+ * Product Class
+ * <b>Hello</b>
+ * <p></p>
+ * @author Harshdeep and Lovepreet
+ * @version 1.1
+ * @since Today's evening
+ */
+
+
+public abstract class Product {
+    protected String name;
+    protected static double coffeePrice = 1.50;
+    protected static double bagelsPrice = 2.50;
+    protected static double donutsPrice = 2.00;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static double getCoffeePrice() {
+        return coffeePrice;
+    }
+
+    public static void setCoffeePrice(double coffeePrice) {
+        Product.coffeePrice = coffeePrice;
+    }
+
+    public static double getBagelsPrice() {
+        return bagelsPrice;
+    }
+
+    public static void setBagelsPrice(double bagelsPrice) {
+        Product.bagelsPrice = bagelsPrice;
+    }
+
+    public static double getDonutsPrice() {
+        return donutsPrice;
+    }
+
+    public static void setDonutsPrice(double donutsPrice) {
+        Product.donutsPrice = donutsPrice;
+    }
+}
